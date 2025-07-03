3e7651a8a59dd407677e5bbfa034543630ed9901
hanklin0804 <hank@instance-20250610-192335.us-central1-b.c.oceanic-oxide-462519-a0.internal>
2025-07-03 17:54:52 +0000

    chore: remove unused related_name

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -9,14 +9,12 @@ class Message(models.Model):
     # 反過來說，一個使用者可以對應到很多則 Message（一對多）
     sender = models.ForeignKey(
         UserLocation,
-        related_name='sent_messages',     # 讓view.py可以寫 user.sent_messages.all()，查這個使用者傳送的所有訊息
         on_delete=models.CASCADE          # 使用者被刪除時，訊息也一併刪除
     )
 
     # 訊息的接收者
     recipient = models.ForeignKey(
         UserLocation,
-        related_name='received_messages', # user.received_messages.all()
         on_delete=models.CASCADE
     )
     
